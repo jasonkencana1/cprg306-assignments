0b40e5dee827934c97bdc33f489fe633d7d99638
jasonkencana1 <jasonalfonza.kencana@edu.sait.ca>
2023-09-20 11:30:20 -0600

    assignment2-finished

---
+++ app/StudentInfo.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function MyComp()
+{
+    return(
+        <div>
+            <h2>Jason Alfonza Kencana</h2>
+            <h2>CPRG-306-E</h2>
+            <Link href="https://github.com/jasonkencana1">Github Repository</Link>
+        </div>
+    );
+}
