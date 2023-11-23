1187f18052d62db0db97c33cbffc1b20221e7624
jasonkencana1 <jasonalfonza.kencana@edu.sait.ca>
2023-11-23 10:34:24 -0700

    week 10 finished

---
+++ week10/page.js
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+
+export default function Page() {
+  return (
+    <main>
+      <Link to="/shopping-list/">Shopping List Login</Link>
+    </main>
+  );
+}
